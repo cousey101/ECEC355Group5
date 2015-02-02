d7470212dc79d95a822bbc27c3d6c2839186ecca
RickAmes <m.purnyn@gmail.com>
2015-02-02 16:17:07 -0500

    fixed or2, and2 mux2 mux3, added mux as components to alu1b

--- ALU1b.vhd
+++ ALU1b.vhd
@@ -5,11 +5,33 @@ use ieee.numeric_std.all;
 
 entity ALU1b is
 port(
-	a,b,aInv,bInv,oper,less,cIn:in std_logic_vector(1 downto 0);
-	result,cOut:out std_logic_vector(1 downto 0));
-  
+	a,b,aInv,bInv,less,cIn:in STD_LOGIC;
+	oper: in STD_LOGIC_VECTOR(1 downto 0);
+	result,cOut:out STD_LOGIC );
 end ALU1b;
 
 ARCHITECTURE beh of ALU1b is
-begin
+  component ADD1b is
+		port(	A, B, cIn:in STD_LOGIC;
+				  Sum, cO :out STD_LOGIC );
+	end component;
+	component muxAinv is
+		port(	x : in STD_LOGIC_VECTOR(1 downto 0);
+				  s : in STD_LOGIC;
+				  y : out STD_LOGIC );
+	end component;
+	component muxBinv is
+		port(	x : in STD_LOGIC_VECTOR(1 downto 0);
+				  s : in STD_LOGIC;
+				  y : out STD_LOGIC );
+	end component;
+	component muxOper is
+		port(	x : in STD_LOGIC_VECTOR(3 downto 0);
+		      s : in STD_LOGIC_VECTOR(1 downto 0);
+				  y : out STD_LOGIC );
+	end component;
+	signal Sum:STD_LOGIC;
+	signal sumCOut:STD_LOGIC;
+begin  
+  ADD:ADD1b port map(A,B,Sum,sumCOut);
 end beh;
