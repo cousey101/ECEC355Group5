a5c450d0e43f86de26a05bfa6911dd8fd9fa34d2
Tajik Choudhury <tc497@cs.drexel.edu>
2015-02-04 14:47:53 -0500

    adding 32 bit alu

--- ALU32b.vhd
+++ ALU32b.vhd
@@ -17,17 +17,20 @@ architecture struc of ALU32b is
         port ( 
             a, b, ainv, binv, less, cin: in STD_LOGIC;
             oper: in STD_LOGIC_VECTOR(1 downto 0);
-            result, cout: out STD_LOGIC);
+            result, cout, set: out STD_LOGIC);
     end component;
 
     signal couts : STD_LOGIC_VECTOR(31 downto 0);
+    signal set : STD_LOGIC_VECTOR(31 downto 0);
 begin
 
 -- less in first one is 
 -- for generate here
-    G1: for i in 1 to 31 generate
-        alui: ALU1b port map (a(i), b(i), ainv, binv, 
+    ALUn:ALU1b port map (a(0), b(0), ainv, binv, set(31), cin, oper, result(0), couts(0), set(0));
+    G1: for i in 1 to 30 generate
+        alui: ALU1b port map (a(i), b(i), ainv, binv, 0, couts(i-1), oper, result(i), couts(i), set(i));  
     end generate G1;
-overflow <= couts(31) xor couts(0);
-
+    ALUlast:ALU1b port map(a(31), b(31), ainv, binv, 0, couts(30), oper, result(31), couts(31), set(31));
+    
+    overflow <= couts(31) xor couts(0);
 end struc;
